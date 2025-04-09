53974b84bcfa8d12182e1f711358e559046a42d6
Varun Mangrulkar <varunman.grulkar@gmail.com>
2025-04-09 23:40:10 +0100

    Installed redux toolkit, created appStore,userSlice.

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,22 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Body from "./Components/Body";
 import Login from "./Components/Login";
 import Profile from "./Components/Profile";
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore";
 
 function App() {
   return (
     <>
-      <BrowserRouter basename="/">
-        <Routes>
-          <Route path="/" element={<Body />}>
-            <Route path="/login" element={<Login />}></Route>
-            <Route path="/profile" element={<Profile />}></Route>
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <Provider store={appStore}>
+        <BrowserRouter basename="/">
+          <Routes>
+            <Route path="/" element={<Body />}>
+              <Route path="/login" element={<Login />}></Route>
+              <Route path="/profile" element={<Profile />}></Route>
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
       {/* <NavBar />
       <h1 className="text-3xl font-bold">Hello World</h1> */}
     </>
