5b04ad859c9fc44633e0ea2ea08e90ccbbc31eae
Varun Mangrulkar <varunman.grulkar@gmail.com>
2025-04-12 18:11:14 +0100

    Refeind the code, made changes in code, after refresh the user stays logged in because of the cookie by calling the /profile/view api

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,8 +1,30 @@
 import Footer from "./Footer";
 import NavBar from "./Navbar";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useEffect } from "react";
 
 const Body = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const fetchUser = async () => {
+    try {
+      const result = await axios.get(BASE_URL + "/profile/view", {
+        withCredentials: true,
+      });
+      dispatch(addUser(result.data));
+      console.log(result.data);
+    } catch (err) {
+      navigate("/login");
+      console.log(err);
+    }
+  };
+  useEffect(() => {
+    fetchUser();
+  }, []);
   return (
     <div>
       <NavBar />
