89d38ca0c4553d4f980e5e88d3f093305f1b0c83
Varun Mangrulkar <varunman.grulkar@gmail.com>
2025-06-23 12:55:23 +0100

    Bug:Fixed the naming convention for build

--- src/Components/Request.jsx
+++ src/Components/Request.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { addRequest } from "../utils/RequestSlice";
+import { addRequest } from "../utils/requestSlice";
 import { BASE_URL } from "../utils/constants";
 
 const Request = () => {
--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
-import requestReducer from "./request_temp";
+import requestReducer from "./requestSlice";
 import feedReducer from "./feedSlice";
 import connectionReducer from "./connectionSlice";
 
