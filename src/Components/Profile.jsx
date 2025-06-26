c7c8323209bd99a761b7fa1d10a62e78791e2901
Varun Mangrulkar <varunman.grulkar@gmail.com>
2025-06-26 23:04:35 +0100

    Bug: Patch request bug fixing

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,16 +1,31 @@
+// import { useSelector } from "react-redux";
+// import { BASE_URL } from "../utils/constants";
+// import UserCard from "./UserCard";
+// import axios from "axios";
+// import { useEffect, useState } from "react";
+// import EditProfile from "./EditProfile";
+
+// const Profile = () => {
+//   const user = useSelector((store) => store.user);
+//   console.log("Redux user state:", user);
+//   return (
+//     user && (
+//       <div className="flex justify-center p-4 ">
+//         <EditProfile user={user} />
+//       </div>
+//     )
+//   );
+// };
+// export default Profile;
+
 import { useSelector } from "react-redux";
-import { BASE_URL } from "../utils/constants";
-import UserCard from "./UserCard";
-import axios from "axios";
-import { useEffect, useState } from "react";
 import EditProfile from "./EditProfile";
 
 const Profile = () => {
   const user = useSelector((store) => store.user);
-  console.log("Redux user state:", user);
   return (
     user && (
-      <div className="flex justify-center p-4 ">
+      <div>
         <EditProfile user={user} />
       </div>
     )
