93a00a5d3f962d191e9ea45c094bed7aeb255fdf
Varun Mangrulkar <varunman.grulkar@gmail.com>
2025-06-27 00:04:27 +0100

    bug:Patch_req_04

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -17,18 +17,3 @@ const Profile = () => {
   );
 };
 export default Profile;
-
-// import { useSelector } from "react-redux";
-// import EditProfile from "./EditProfile";
-
-// const Profile = () => {
-//   const user = useSelector((store) => store.user);
-//   return (
-//     user && (
-//       <div>
-//         <EditProfile user={user} />
-//       </div>
-//     )
-//   );
-// };
-// export default Profile;
